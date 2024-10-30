c163ad6a5637329d500ddf41147a32a9e02c7559
Patrick Hennessey <pjhennessey89@gmail.com>
2024-10-29 21:17:36 -0500

    tested data on graphQL and fixed some bugs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,10 +1,13 @@
-const typeDefs = `
+const { gql } = require('apollo-server-express');
+
+const typeDefs = gql`
   type User {
     _id: ID
     firstName: String
     lastName: String
     email: String
-    avatar: String
+    avatarUrl: String
+    avatarStyle: String
     stats: UserStats
     preferences: Preferences
     friends: [User]
@@ -40,7 +43,7 @@ const typeDefs = `
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
+    addUser(firstName: String!, lastName: String!, email: String!, password: String!, avatarStyle: String!): Auth
     updateUser(firstName: String, lastName: String, email: String, password: String): User
     login(email: String!, password: String!): Auth
     createGame: Game
