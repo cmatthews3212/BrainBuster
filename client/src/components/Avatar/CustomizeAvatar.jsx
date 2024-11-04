a3b920c117a79559f5e6ecb5b7a8215ce33946a1
Christine Matthews <candreola@gmail.com>
2024-11-04 14:05:47 -0600

    add none options

--- client/src/components/Avatar/CustomizeAvatar.jsx
+++ client/src/components/Avatar/CustomizeAvatar.jsx
@@ -9,14 +9,14 @@ import { adventurer, glass } from '@dicebear/collection';
 
 
 const CustomizeAvatar = ({ src, name, onBack }) => {
-    const hairSeeds = ["long01", "long02", "long03", "long04", "long05","long06","long07","long08", "long09" , "long10" , "long11" , "long12" , "long13","long14","long15","long16","long17","long18","long19","long20","long21","long22","long23","long24","long25","long26","short01","short02","short03","short04","short05","short06","short07","short08","short09","short10","short11","short12","short13","short14","short15","short16" , "short17" , "short18" , "short19"]
+    const hairSeeds = ["long00", "long01", "long02", "long03", "long04", "long05","long06","long07","long08", "long09" , "long10" , "long11" , "long12" , "long13","long14","long15","long16","long17","long18","long19","long20","long21","long22","long23","long24","long25","long26","short01","short02","short03","short04","short05","short06","short07","short08","short09","short10","short11","short12","short13","short14","short15","short16" , "short17" , "short18" , "short19"]
     const hairColorSeeds = ["0e0e0e","3eac2c","6a4e35","85c2c6","796a45","562306","592454","ab2a18","ac6511","afafaf","b9a05f","cb6820","dba3be","e5d7a3"]
     const mouthSeeds = ["variant01","variant02", "variant03","variant04","variant05","variant06","variant07","variant08","variant09","variant10","variant11","variant12","variant13","variant14","variant15","variant16","variant17","variant18","variant19","variant20","variant21","variant22","variant23","variant24","variant25","variant26","variant27","variant28","variant29","variant30"]
     const eyesSeeds = ["variant01","variant02","variant03","variant04","variant05","variant06","variant07","variant08","variant09","variant10" ,"variant11" , "variant12" , "variant13" , "variant14" , "variant15" , "variant16" , "variant17" , "variant18", "variant19" , "variant20" , "variant21"  , "variant22" , "variant23" , "variant24" , "variant25" , "variant26"]
     const eyebrowSeeds = ["variant01","variant02","variant03","variant04", "variant05","variant06","variant07","variant08","variant09","variant10","variant11", "variant12", "variant13" , "variant14" , "variant15"]
-    const earringsSeeds = ["variant01","variant02","variant03","variant04","variant05","variant06"]
-    const featuresSeeds = ["birthmark","blush","freckles","mustache"]
-    const glassesSeeds = ["variant01","variant02","variant03","variant04","variant05"]
+    const earringsSeeds = ["variant00", "variant01","variant02","variant03","variant04","variant05","variant06"]
+    const featuresSeeds = ["variant00", "birthmark","blush","freckles","mustache"]
+    const glassesSeeds = ["variant00", "variant01","variant02","variant03","variant04","variant05"]
     if (!src) return null;
 
     const [currentSrc, setCurrentSrc] = useState(src)
@@ -346,6 +346,10 @@ const CustomizeAvatar = ({ src, name, onBack }) => {
         const handleToggleGlasses = () => {
             setGlasses(!glasses)
         }
+
+        const handleNoEarrings = () => {
+            setEarringsProb(0)
+        }
        
 
   
@@ -372,6 +376,7 @@ const CustomizeAvatar = ({ src, name, onBack }) => {
                 {hairArray.map((hair) => (
                         <img  onClick={() => handleHairClick(hair.hair)}  name={hair.seed} className='avatar-hair-list' src={hair.src}></img>
                     ))} 
+                    
 
                 </div>
                 )}
@@ -422,7 +427,7 @@ const CustomizeAvatar = ({ src, name, onBack }) => {
                 {earringsArray.map((earrings) => (
                         <img  onClick={() => handleEarringsClick(earrings.earrings)}  name={earrings.seed} className='avatar-hair-list' src={earrings.src}></img>
                     ))} 
-
+                  
                 </div>
                 )}
                    <button className='customizeBtn' onClick={handleToggleFeatures}>{features ? 'Select' : 'Choose'} Features </button>
