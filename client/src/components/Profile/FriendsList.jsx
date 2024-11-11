057ea1dd4339c5a0ccdfc9bd04ae5ebeb88997e4
CatMamaGB <165972971+CatMamaGB@users.noreply.github.com>
2024-11-11 15:42:43 -0600

    Update profile display

---
+++ client/src/components/Profile/FriendsList.jsx
@@ -0,0 +1,34 @@
+import { Link } from 'react-router-dom';
+
+const FriendsList = ({ userData, handleRemoveFriend, navigate }) => {
+    return (
+        <div className='friends-list'>
+            <h2>Your Friends</h2>
+            <button onClick={() => navigate('/find')} className="find-friends-btn">
+                Find Friends!
+            </button>
+            <div className='friends-grid'>
+                {userData?.friends ? (
+                    userData.friends.map((friend) => (
+                        <div key={friend._id} className='friend-card'>
+                            <h3>{friend.firstName} {friend.lastName}</h3>
+                            <hr />
+                            <div className='friend-actions'>
+                                <Link className='view-friend' to={`/profile/${friend._id}`}>
+                                    View Friend
+                                </Link>
+                                <button className="remove-friend" onClick={() => handleRemoveFriend(friend)}>
+                                    Remove Friend
+                                </button>
+                            </div>
+                        </div>
+                    ))
+                ) : (
+                    <p>No friends found...</p>
+                )}
+            </div>
+        </div>
+    );
+}
+
+export default FriendsList; 
