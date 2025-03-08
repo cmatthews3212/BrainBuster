1288347f285194634f90dffe015380cce8112fb3
Christine Matthews <candreola@gmail.com>
2025-03-07 18:26:18 -0600

    update url

--- client/src/socket.js
+++ client/src/socket.js
@@ -1,6 +1,6 @@
 import { io } from "socket.io-client";
 
-const SOCKET_IO_URL = "https://brainbuster-12xu.onrender.com/"; 
+const SOCKET_IO_URL = "https://brainbuster-12xu.onrender.com"; 
 
 const socket = io(SOCKET_IO_URL, {
   autoConnect: false, 
